50993da54a82e8e5d19219c14b3ea91a16226653
Joncoded <joncoded@gmail.com>
2023-11-23 14:13:36 -0500

    making initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Component from './Component.js'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello!</h1>
+      <h2>Let's begin our React App!</h2>
+      <Component />
+    </div>
+  );
+}
