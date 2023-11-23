242a01fc5d2237654764f7ffbec670b4a0a56fe3
Joncoded <joncoded@gmail.com>
2023-11-23 15:19:23 -0500

    adding alternate props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import Component from './Component.js'
 import { Component } from './Component.js'
+import { ComponentAlternate } from './ComponentAlternate.js'
 
 export default function App() {
   return (
     <div>
       <h1>Hello!</h1>
       <h2>Let's begin our React App!</h2>
-      <Component />
       <Component someData="hello component" />
+      <ComponentAlternate someData="alternate component" />
     </div>
   );
 }
---
+++ src/ComponentAlternate.js
@@ -0,0 +1,8 @@
+export function ComponentAlternate({someData}) {
+  return (
+    <div>
+      <h3>{someData}</h3>
+      <p>A child component!</p>      
+    </div>
+  );
+}
